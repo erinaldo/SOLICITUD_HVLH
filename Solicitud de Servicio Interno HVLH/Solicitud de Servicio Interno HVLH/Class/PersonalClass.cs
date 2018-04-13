98e46e0a2f5d5262abf5c328135f873819381f25
Armando Carbajal <jeffant.13@gmail.com>
2018-04-13 11:00:39 -0500

    Iniciando

---
+++ Solicitud de Servicio Interno HVLH/Solicitud de Servicio Interno HVLH/Class/PersonalClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solicitud_de_Servicio_Interno_HVLH.Class
+{
+    public class PersonalClass
+    {
+        public int MyProperty { get; set; }
+
+
+        
+    }
+}
